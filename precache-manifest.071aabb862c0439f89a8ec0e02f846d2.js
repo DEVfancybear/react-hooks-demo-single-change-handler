d49940add8285ecd510aa0e391441f9baaf034c8
DEVfancybear <duonghacker2345@gmail.com>
2020-03-27 16:31:44 +0700

    Updates

---
+++ precache-manifest.071aabb862c0439f89a8ec0e02f846d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9548f8d9e1f516402bcae81914c4de53",
+    "url": "/DEVfancybear/react-hooks-demo-single-change-handler/index.html"
+  },
+  {
+    "revision": "6cac27705cc9132e0517",
+    "url": "/DEVfancybear/react-hooks-demo-single-change-handler/static/css/main.ca140e72.chunk.css"
+  },
+  {
+    "revision": "fdacfb9e729f4c2c12c1",
+    "url": "/DEVfancybear/react-hooks-demo-single-change-handler/static/js/2.e5cfb2c4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/DEVfancybear/react-hooks-demo-single-change-handler/static/js/2.e5cfb2c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cac27705cc9132e0517",
+    "url": "/DEVfancybear/react-hooks-demo-single-change-handler/static/js/main.03c594c4.chunk.js"
+  },
+  {
+    "revision": "6f348eae2a30b10fde34",
+    "url": "/DEVfancybear/react-hooks-demo-single-change-handler/static/js/runtime-main.92b70347.js"
+  }
+]);
